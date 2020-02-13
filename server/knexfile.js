2338dd7da96ea4e25cdae832ebf3e1dba53a7b15
Colton Crouch <colton.crouch96@gmail.com>
2020-02-12 23:09:11 -0500

    Update knex config

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,3 +1,6 @@
+// Read config here so migration cli works
+require('dotenv').config();
+
 module.exports = {
 
   development: {
@@ -7,36 +10,14 @@ module.exports = {
     }
   },
 
-  staging: {
+  testing: {
     client: 'postgresql',
-    connection: {
-      database: process.env.DB_NAME,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    connection: process.env.DB_CONNECTION_STRING
   },
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: process.env.DB_NAME,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    connection: process.env.DB_CONNECTION_STRING
   }
 
 };

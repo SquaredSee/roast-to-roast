a88c68167376da72c100b7324947d0e77942d32d
Colton Crouch <colton.crouch96@gmail.com>
2020-02-18 18:40:04 -0500

    Add log seed entries

--- server/seeds/03_logs.js
+++ server/seeds/03_logs.js
@@ -1,9 +1,21 @@
-const logs = [
-  {
-  }
-];
-
 exports.seed = function(knex) {
+  const logs = [
+    {
+      coffee: 'GOOD BEANS',
+      description: 'These are some really good beans.',
+      rating: 5,
+      shop_id: knex('shops').where('name', 'Roaster One').select('id'),
+      user_id: knex('users').where('username', 'jdoe123').select('id'),
+    },
+    {
+      coffee: 'Latte',
+      description: 'Good!!!',
+      rating: 5,
+      shop_id: knex('shops').where('name', 'Cafe One').select('id'),
+      user_id: knex('users').where('username', 'jdoe456').select('id'),
+    }
+  ];
+
   // Deletes ALL existing entries
   return knex('logs').del()
     .then(() => {

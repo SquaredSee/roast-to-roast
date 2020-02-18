f991d682c3903104c936291991b512431070f74e
Colton Crouch <colton.crouch96@gmail.com>
2020-02-18 15:53:35 -0500

    Remove index route references

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -14,7 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;

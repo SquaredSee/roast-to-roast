8a04e40e7f36712f95341b960e98b206dd3e0208
Colton Crouch <colton.crouch96@gmail.com>
2020-02-22 23:47:18 -0500

    Add /users/:id route

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,15 +4,28 @@ const users = require('../db/users-queries');
 
 const router = express.Router();
 
+const isValidId = (req, res, next) => {
+  // Middleware to validate IDs
+  if (!isNaN(req.params.id)) {
+    return next();
+  }
+  next(new Error('Invalid ID'));
+};
+
 router.get('/', (_req, res, _next) => {
   users.all().then((users) => {
     res.json(users);
   });
 });
 
-router.get('/a', (_req, res, _next) => {
-  users.whereUsername('jdoe123').then((users) => {
-    res.json(users);
+router.get('/:id', isValidId, (req, res, next) => {
+  users.whereId(req.params.id).first().then((id) => {
+    if (id) {
+      res.json(id);
+    }
+    else {
+      next();
+    }
   });
 });
 

b7d98972ca030be39c6773a0aa1bf9a058dcf67a
Colton Crouch <colton.crouch96@gmail.com>
2020-03-31 21:18:00 -0400

    Copy users route code for shops

--- server/routes/shops.js
+++ server/routes/shops.js
@@ -1,12 +1,64 @@
 const express = require('express');
 
-const queries = require('../db/shops-queries');
+const shops = require('../db/shops-queries');
 
 const router = express.Router();
 
+const isValidId = (req, res, next) => {
+  // Middleware to validate IDs
+  if (!isNaN(req.params.id)) {
+    return next();
+  }
+  next(new Error('Invalid ID'));
+};
+
+const isValidShop = (user) => {
+  const location = typeof user.location === 'string' && user.location.trim() !== '';
+  return location && true;
+};
+
 router.get('/', (_req, res, _next) => {
-  queries.getAll().then((users) => {
-    res.json(users);
+  shops.all().then((s) => {
+    res.json(s);
+  });
+});
+
+router.get('/:id', isValidId, (req, res, next) => {
+  shops.whereId(req.params.id).then((id) => {
+    if (id) {
+      res.json(id);
+    }
+    else {
+      next();
+    }
+  });
+});
+
+router.post('/', (req, res, next) => {
+  if (isValidShop(req.body)) {
+    shops.create(req.body).then((s) => {
+      res.json(s[0]);
+    });
+  }
+  else {
+    next(new Error('Invalid shop'));
+  }
+});
+
+router.put('/:id', isValidId, (req, res, next) => {
+  if (isValidShop(req.body)) {
+    shops.update(req.params.id, req.body).then((s) => {
+      res.json(s);
+    });
+  }
+  else {
+    next(new Error('Invalid shop'));
+  }
+});
+
+router.delete('/:id', isValidId, (req, res, next) => {
+  shops.delete(req.params.id).then(() => {
+    res.json({'deleted': true});
   });
 });
 

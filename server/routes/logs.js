c2f56f115686dc7b724206a2e26d77a9753972af
Colton Crouch <colton.crouch96@gmail.com>
2020-04-08 17:25:37 -0400

    Allow ratings up to 10

--- server/routes/logs.js
+++ server/routes/logs.js
@@ -15,7 +15,7 @@ const isValidId = (req, res, next) => {
 const isValidLog = (log) => {
   // const shop_id = typeof log.shop_id === 'number' && log.shop_id >= 0;
   const user_id = typeof log.user_id === 'number' && log.user_id >= 0;
-  const rating = typeof log.rating === 'number' && log.rating > 0 && log.rating < 6;
+  const rating = typeof log.rating === 'number' && log.rating > 0 && log.rating < 11;
   const coffee = typeof log.coffee === 'string' && log.coffee.trim() !== '';
   const tasting_note_1 = typeof log.tasting_note_1 === 'string' && log.tasting_note_1.trim() !== '';
   return user_id && rating && coffee && tasting_note_1;

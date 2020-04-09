1370f6d2eb7db8e3ddb4d912bcbb15db2c2a37e7
Colton Crouch <colton.crouch96@gmail.com>
2020-04-08 20:05:03 -0400

    Connect taste page to server

--- app/screens/Taste.js
+++ app/screens/Taste.js
@@ -1,30 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import {View, Text, Image, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet, Button} from 'react-native';
 import { TouchableOpacity, ScrollView } from 'react-native-gesture-handler';
 
 import Colors from '../constants/Colors';
 
-export default function tasteNotes(props){
-  return(
-    <View style={styles.container}>
-      <View style={styles.createButtonContainer}>
-        <Text style={ styles.addNewText}>Add New</Text>
-        <TouchableOpacity onPress={() => props.navigation.navigate('NewTaste')}>
-          <Image source={require('../assets/icons/plusButton.png')} style={{width: 40, height: 40}}></Image>
-        </TouchableOpacity>
+const objectToQueryString = (obj) => {
+  return Object.keys(obj).map((key) => key + '=' + obj[key]).join('&');
+};
+
+export default class TasteNotes extends Component {
+
+  constructor(props) {
+    super(props);
+    this.getLogs = this.getLogs.bind(this);
+    this.state = { logs: [] };
+  }
+
+  getLogs() {
+
+    let q = '';
+    q = '?' + objectToQueryString({user_id: 1});  // TODO: Add authentication?
+
+    fetch(`http://192.168.1.4:3000/logs${q}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.length > 0) {
+        this.setState({ logs: data });
+      }
+    })
+    .catch((e) => console.error(e));
+  }
+
+  render() {
+    return(
+      <View style={styles.container}>
+        <View style={styles.createButtonContainer}>
+          <Text style={ styles.addNewText}>Add New</Text>
+          <TouchableOpacity onPress={() => this.props.navigation.navigate('NewTaste')}>
+            <Image source={require('../assets/icons/plusButton.png')} style={{width: 40, height: 40}}></Image>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.noteListContainer}>
+          <Button onPress={this.getLogs} title="press me ;)" />
+          <Text>Your Notes</Text>
+          <ScrollView>
+            {this.state.logs.map((log) => <Text key={log.id}>{log.coffee}</Text>)}
+          </ScrollView>
+        </View>
       </View>
-      <View style={styles.noteListContainer}>
-        <Text>Your Notes</Text>
-        <ScrollView>
-          <Text>Testing</Text>
-        </ScrollView>
-      </View>
-    </View>
-  );
+    );
+  }
 }
 
-tasteNotes.navigationOptions = {
+TasteNotes.navigationOptions = {
   headerTitle: 'Taste',
   headerTitleStyle: {
     fontFamily: 'knockout46',

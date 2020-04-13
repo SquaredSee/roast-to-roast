41c0d0f8476c4ddef5146d986ee940dfa3e3a7ba
Colton Crouch <colton.crouch96@gmail.com>
2020-04-13 16:52:23 -0400

    Install react-native-maps, set up initial MapView

--- app/screens/Find.js
+++ app/screens/Find.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, Platform } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, View, Platform, Dimensions } from 'react-native';
 
 import Colors from '../constants/Colors';
 
 export default class Find extends Component {
+  constructor(props) {
+    super(props);
+    this.handleRegionChange = this.handleRegionChange.bind(this);
+    this.state = {
+      region: {
+        latitude: 39.10,
+        longitude: -84.51,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.05
+      },
+      markers: [
+      ]
+    };
+  }
+
+  handleRegionChange(region) {
+    this.setState({ region });
+  }
+
   render() {
     return(
       <View style={styles.mainContainer}>
-        <Text>Testing</Text>
+        <MapView
+          style={styles.mapStyle}
+          region={this.state.region}
+          onRegionChangeComplete={this.handleRegionChange}
+        >
+          {this.state.markers.map((marker) => (
+            <Marker
+              key={marker.id}
+              coordinate={marker.latlong}
+              title={marker.title}
+              description={marker.description}
+            />
+          ))}
+        </MapView>
       </View>
     );
   }
@@ -34,7 +67,12 @@ const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
     alignItems: 'center',
+    justifyContent: 'center',
     paddingTop: (Platform.OS === 'ios') ? 20 : 0,
     backgroundColor: Colors.mandy,
   },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
 });

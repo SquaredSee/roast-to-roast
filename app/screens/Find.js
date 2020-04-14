957c27033778047837ce6ebe31fc7a9e783e4241
Colton Crouch <colton.crouch96@gmail.com>
2020-04-14 16:12:45 -0400

    Update map style

--- app/screens/Find.js
+++ app/screens/Find.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, View, Platform } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import { StyleSheet, View, Platform, Text } from 'react-native';
 
 import Colors from '../constants/Colors';
 
@@ -25,7 +25,8 @@ const buildMarkerList = (data) => (
     id: shop.id,
     title: shop.name,
     coordinate: latlng(shop.latitude, shop.longitude),
-    description: describe(shop)
+    description: describe(shop),
+    color: `#${Math.floor(Math.random()*16777215).toString(16)}`
   }))
 );
 
@@ -76,6 +77,7 @@ export default class Find extends Component {
           style={styles.mapStyle}
           region={this.state.region}
           onRegionChangeComplete={this.handleRegionChange}
+          customMapStyle={mapStyle}
         >
           {this.state.markers.map((marker) => (
             <Marker
@@ -83,7 +85,10 @@ export default class Find extends Component {
               coordinate={marker.coordinate}
               title={marker.title}
               description={marker.description}
-            />
+              pinColor={marker.color}
+            >
+              {/* <Callout>custom map pin tooltip, can be any react component</Callout> */}
+            </Marker>
           ))}
         </MapView>
       </View>
@@ -120,3 +125,220 @@ const styles = StyleSheet.create({
     height: '100%'
   }
 });
+
+// Default 'Retro' style from https://mapstyle.withgoogle.com/
+const mapStyle = [
+  {
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#ebe3cd'
+      }
+    ]
+  },
+  {
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#523735'
+      }
+    ]
+  },
+  {
+    'elementType': 'labels.text.stroke',
+    'stylers': [
+      {
+        'color': '#f5f1e6'
+      }
+    ]
+  },
+  {
+    'featureType': 'administrative',
+    'elementType': 'geometry.stroke',
+    'stylers': [
+      {
+        'color': '#c9b2a6'
+      }
+    ]
+  },
+  {
+    'featureType': 'administrative.land_parcel',
+    'elementType': 'geometry.stroke',
+    'stylers': [
+      {
+        'color': '#dcd2be'
+      }
+    ]
+  },
+  {
+    'featureType': 'administrative.land_parcel',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#ae9e90'
+      }
+    ]
+  },
+  {
+    'featureType': 'landscape.natural',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#dfd2ae'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#dfd2ae'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#93817c'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi.park',
+    'elementType': 'geometry.fill',
+    'stylers': [
+      {
+        'color': '#a5b076'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi.park',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#447530'
+      }
+    ]
+  },
+  {
+    'featureType': 'road',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#f5f1e6'
+      }
+    ]
+  },
+  {
+    'featureType': 'road.arterial',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#fdfcf8'
+      }
+    ]
+  },
+  {
+    'featureType': 'road.highway',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#f8c967'
+      }
+    ]
+  },
+  {
+    'featureType': 'road.highway',
+    'elementType': 'geometry.stroke',
+    'stylers': [
+      {
+        'color': '#e9bc62'
+      }
+    ]
+  },
+  {
+    'featureType': 'road.highway.controlled_access',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#e98d58'
+      }
+    ]
+  },
+  {
+    'featureType': 'road.highway.controlled_access',
+    'elementType': 'geometry.stroke',
+    'stylers': [
+      {
+        'color': '#db8555'
+      }
+    ]
+  },
+  {
+    'featureType': 'road.local',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#806b63'
+      }
+    ]
+  },
+  {
+    'featureType': 'transit.line',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#dfd2ae'
+      }
+    ]
+  },
+  {
+    'featureType': 'transit.line',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#8f7d77'
+      }
+    ]
+  },
+  {
+    'featureType': 'transit.line',
+    'elementType': 'labels.text.stroke',
+    'stylers': [
+      {
+        'color': '#ebe3cd'
+      }
+    ]
+  },
+  {
+    'featureType': 'transit.station',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#dfd2ae'
+      }
+    ]
+  },
+  {
+    'featureType': 'water',
+    'elementType': 'geometry.fill',
+    'stylers': [
+      {
+        'color': '#b9d3c2'
+      }
+    ]
+  },
+  {
+    'featureType': 'water',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#92998d'
+      }
+    ]
+  }
+];
